51080d48fc71ae34c53ef024b64ce5e85a751337
dis-card <vikash_98352@yahoo.co.in>
2014-02-14 19:49:19 +0530

    Added Sql Script to the project

---
+++ Scripts/Script.sql
@@ -0,0 +1,53 @@
+
+CREATE TABLE  Customer  (
+   firstName  VARCHAR(50),
+   lastName  VARCHAR(50),
+   dateOfBirth  DATE,
+   streetName  VARCHAR(25),
+   streetNumber  INTEGER,
+   city  VARCHAR(25),
+   pin  CHAR(6),
+   id  INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  PRIMARY KEY  ( id )
+);
+
+
+
+CREATE TABLE  City  (
+   acronym  VARCHAR(4) NOT NULL,
+   name  VARCHAR(20),
+  PRIMARY KEY  ( acronym )
+);
+
+CREATE TABLE  RegularSavingsAccount  (
+   customerId  INTEGER,
+   accountNo  INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+   cityAcronym  VARCHAR(4),
+   initialDeposit  DOUBLE,
+   openingDate  DATE,
+  PRIMARY KEY  ( accountNo )
+);
+
+CREATE TABLE  SalarySavingsAccount  (
+   customerId  INTEGER,
+   accountNo  INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+   cityAcronym  VARCHAR(4),
+   initialDeposit  DOUBLE,
+   openingDate  DATE,
+  PRIMARY KEY  ( accountNo )
+);
+
+
+
+
+
+ALTER TABLE  RegularSavingsAccount  ADD CONSTRAINT  RegularSavingsAccount_fk1  FOREIGN KEY ( customerId ) REFERENCES Customer( id );
+ALTER TABLE  RegularSavingsAccount  ADD CONSTRAINT  RegularSavingsAccount_fk2  FOREIGN KEY ( cityAcronym ) REFERENCES City( acronym );
+ALTER TABLE  SalarySavingsAccount  ADD CONSTRAINT  SalarySavingsAccount_fk1  FOREIGN KEY ( customerId ) REFERENCES Customer( id );
+ALTER TABLE  SalarySavingsAccount  ADD CONSTRAINT  SalarySavingsAccount_fk2  FOREIGN KEY ( cityAcronym ) REFERENCES City( acronym );
+ALTER TABLE  EmployeeSalarySavings  ADD CONSTRAINT  EmployeeSalarySavings_fk1  FOREIGN KEY ( customerId ) REFERENCES Customer( id );
+ALTER TABLE  EmployeeSalarySavings  ADD CONSTRAINT  EmployeeSalarySavings_fk2  FOREIGN KEY ( employeeId ) REFERENCES Employee( id );
+ALTER TABLE  EmployeeSalarySavings  ADD CONSTRAINT  EmployeeSalarySavings_fk3  FOREIGN KEY ( cityAcronym ) REFERENCES City( acronym );
+
+insert into vikash.city values ('BLR','Bangalore');
+insert into vikash.city values ('MUM','Mumbai');
